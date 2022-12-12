a88e2592bba8de7baea1526655b9c06bb5f64c64
“Bethany <Bethanygellington@gmail.com>
2022-12-12 21:49:05 +0800

    commitment issues

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,3 +1,4 @@
+import { BindingType, parseTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,16 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+  memberBeingEdited: object = null;
+  inCrew: boolean = false;
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
+    
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  // below throws alert 3x if we keep the toUpperCase part. how to fix?
+  add(memberName: string, isFirst: boolean) {
+    for (let i=0; i < this.crew.length; i++) {
+        if(this.crew[i]['name'].toUpperCase() === memberName.toUpperCase()) {
+            this.inCrew = true;
+            alert("You already have this crew member")
+        }  
+    }
+   
+    if (!this.inCrew && memberName !== "") {
+        this.crew.push({name: memberName, firstMission: isFirst});
+    }
+    
+    this.inCrew = false;
+  }
+      
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
   }
 
 }
